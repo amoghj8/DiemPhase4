0626d6355c772d45ad2d230fa732f03c72d113d7
Amogh Joshi <amoghj8@gmail.com>
2021-11-28 21:10:34 -0500

    Added partitionGeneartor implementation

--- Generator.py
+++ Generator.py
@@ -1,7 +1,7 @@
 from config import configs
 
-class TestGenerator:
-    
+class TestGenerator():
+
     # l : boolean flag ( True : all nodes can be leader, False : Only Faulty Nodes can be leader )
     # nodes : list of nodes excluding the twins
     # node_to_twin_dict : Mapping from node to twin ( if it exists )
@@ -48,8 +48,46 @@ class TestGenerator:
         nodes_list = [chr(i) for i in range(ord('A'),ord('A')+n)]
         return nodes_list
 
-    
+    # To partition input of (x, y) where x is the number of input elements and y is the number of partitions, we can do that in two steps: 
+    # Scenario 1: Partition the input (x-1, y-1) and add xth element to another partition
+    # Scenario 2: Partition the input (x-1, y) and add xth element to one of the y partitions. So, this can be done y times
+    def partitionGenerator(inputList, numberOfPartitions):
+        result = []
 
+        if len(inputList) < numberOfPartitions or  numberOfPartitions < 1 :
+            return result
+
+        if numberOfPartitions == 1:
+            partition = []
+            partition.append(inputList[:])
+            result.append(partition)
+            return result
+
+        sets = []
+        sets.append(inputList[len(inputList)-1])
+
+        #Partition the input to size (x-1, y-1)
+        secondPartition = TestGenerator.partitionGenerator(inputList[0:len(inputList)-1], numberOfPartitions-1)
+
+        for i in range(len(secondPartition)):
+            tempList = secondPartition[i]
+            l = tempList[:]
+            l.append(sets)
+            result.append(l)
+
+        # Partition the elements to size (x-1, y) by calling the function recursively   
+        firstPartition = TestGenerator.partitionGenerator(inputList[0:len(inputList)-1], numberOfPartitions)
+
+        for i in range(len(firstPartition)):
+            for j in range(len(firstPartition[i])):
+                l = []
+                for lst in firstPartition[i]:
+                    l.append(lst[:])
+                l[j].append(inputList[len(inputList)-1])
+                result.append(l)
+                
+        return result    
+    
 if __name__ == '__main__':
     
     # -------------------------------- Test get_leader ----------------------------------------
@@ -76,3 +114,15 @@ if __name__ == '__main__':
     
     for config in configs:
         tg.setup(config)
+
+    # -----------------------------------------------------------------------------------------
+    # Test PartitionGenerator
+    inputList = []
+    for i in range(1,5):
+        inputList.append(i)
+    numberOfPartitions = 2
+    partitionList = TestGenerator.partitionGenerator(inputList, numberOfPartitions)
+    print("Generated Partitions ", partitionList)
+    print(partitionList)
+    if (len(partitionList) != 7):
+        print("Partition generation Missing")
